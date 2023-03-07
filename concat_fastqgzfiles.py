a73a96145bf7470c6a64be5d2fe15d7e2e5e03df
Rasmus Kirkegaard <rasmus.kirkegaard@hotmail.com>
2023-03-07 14:51:36 +0100

    Update concat_fastqgzfiles.py

--- concat_fastqgzfiles.py
+++ concat_fastqgzfiles.py
@@ -53,11 +53,12 @@ for variable in variables_to_check:
 # Create runid        
 runid_val = f'{start_time_value}_np_{flow_cell_id}'
 
-# Prepare
+# Prepare folder structure
 if os.path.exists(f'{output_dir}/fastq'):
     print("The folder 'fastq' exists.")
 else:
     print("The folder 'fastq' does not exist.")
+    os.mkdir(f'{output_dir}/fastq')
 
 # Check if barcodes have been used 
 fastq_files = glob.glob(os.path.join(fastq_pass_dir, "*fastq.gz"))
