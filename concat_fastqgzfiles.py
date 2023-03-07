db3039a0172386837658decbc909a6daa1d711d5
Rasmus Kirkegaard <rasmus.kirkegaard@hotmail.com>
2023-03-07 14:58:18 +0100

    Update concat_fastqgzfiles.py

--- concat_fastqgzfiles.py
+++ concat_fastqgzfiles.py
@@ -84,9 +84,9 @@ for barcode in barcodes:
     
     output_file = os.path.join(output_dir, f'{runid_val}.{barcode}.g{guppy_version}.{guppy_filename_value}.fastq.gz')
     if os.path.isfile(output_file):
-    print("File exists")
+        print("File exists")
     else:
-    print("File does not exist")
-    command = f'cat {barcode_dir}/*.fastq.gz > {output_file}'
-    print(command)
-    subprocess.run(command, shell=True)
+        print("File does not exist")
+        command = f'cat {barcode_dir}/*.fastq.gz > {output_file}'
+        print(command)
+        subprocess.run(command, shell=True)
