d515b47b7209b9397c713c93b532bd2c217566cb
UR36RV <ur36rv@bio.aau.dk@a512-gc1-e-01.srv.aau.dk>
2023-03-15 14:26:15 +0100

    added functionality to collect reports

---
+++ move_reports.py
@@ -0,0 +1,41 @@
+import subprocess
+import os
+import json
+import sys
+import glob
+
+input_dir = "test_data/2022-12-22_np_PAG65826/"
+output_dir = "out_put/reports/"
+
+# Locate json file and the fastq_pass directory
+json_files = []
+# Loop through all directories and files in the specified directory
+for root, dirs, files in os.walk(input_dir):
+    for file in files:
+            if file.endswith('.json'):
+                json_files.append(os.path.join(root, file))
+    # Check if the "fastq_pass" directory is in the list of directories
+    if "fastq_pass" in dirs:
+        # If it is, save the path to the "fastq_pass" directory to a variable
+        run_dir = root
+        print(root)
+        fastq_pass_dir = os.path.join(root, "fastq_pass")
+        break
+
+def move_files(input_dir, output_dir, file_extension):
+     command = f'cp {input_dir}/*.{file_extension} {output_dir}/'
+     subprocess.run(command, shell=True)
+
+# Prepare folder structure
+if os.path.exists(f'{output_dir}'):
+    print(f'The folder {output_dir} exists.')
+else:
+    print(f'The folder {output_dir} does not exist.')
+    os.mkdir(f'{output_dir}/')
+
+# Define the variables to check
+file_extensions = [ 'txt', 'md', 'csv', 'json', 'html', 'tsv']
+
+# Loop over the variables and check if they are defined
+for f_ext in file_extensions:
+     move_files(f'{run_dir}', f'{output_dir}', f'{f_ext}')
